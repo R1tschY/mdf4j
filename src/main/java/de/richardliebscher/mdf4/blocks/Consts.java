04dc15dcc5c8d503ad69744c1441df51f408e7ee
Richard Liebscher <r1tschy@posteo.de>
2024-03-09 18:46:37 +0100

    Fix detection of unfinished files

--- src/main/java/de/richardliebscher/mdf4/blocks/Consts.java
+++ src/main/java/de/richardliebscher/mdf4/blocks/Consts.java
@@ -8,7 +8,7 @@ package de.richardliebscher.mdf4.blocks;
 public class Consts {
 
   public static final String FILE_MAGIC = "MDF     ";
-  public static final String UNFINISHED_FILE_MAGIC = "UnFinMDF ";
+  public static final String UNFINISHED_FILE_MAGIC = "UnFinMF ";
 
   public static final long HD_BLOCK_OFFSET = 64;
 }
